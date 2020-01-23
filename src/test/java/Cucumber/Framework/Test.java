efdbee9b08a8b4ea2f441113314f63841b53ba65
madhus10 <pinkey.madhu@gmail.com>
2020-01-24 01:45:04 +0530

    second commit

--- src/test/java/Cucumber/Framework/Test.java
+++ src/test/java/Cucumber/Framework/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("first line added");
-
+		System.out.println("second line added");
 	}
 
 }
